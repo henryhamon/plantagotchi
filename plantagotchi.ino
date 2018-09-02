3e5e1600878c45f824aa65dfb8902813ae3ca92b
Henry Hamon <henryhamon@gmail.com>
2018-09-02 12:04:34 -0300

    Add emotions and Soil Moisture

--- plantagotchi.ino
+++ plantagotchi.ino
@@ -9,6 +9,7 @@
 
 LCD5110 lcd(8,9,10,12,11);
 
+// Emotions Graphic
 extern uint8_t bad[];
 extern uint8_t crying[];
 extern uint8_t dead[];
@@ -16,54 +17,79 @@ extern uint8_t disappointed[];
 extern uint8_t grumpy[];
 extern uint8_t happy[];
 extern uint8_t sad[];
-extern uint8_t wound[];
+
+int soilMoisture;
+int sensorPin = A0;
 
 void setup()
 {
   lcd.InitLCD();
 }
 
-void loop()
+void readSoilMoisture()
+{
+  int sensorValue = analogRead(sensorPin); // read the raw value
+  soilMoisture = map(sensorValue,550,0,0,100); // map the received value
+}
+
+void feelingHappy()
+{
+  lcd.drawBitmap(0,0,happy,84,48);
+}
+
+void feelingGrumpy()
+{
+  lcd.drawBitmap(0,0,grumpy,84,48);
+}
+
+void feelingBad()
+{
+  lcd.drawBitmap(0,0,bad,84,48);
+}
+
+void feelingDisappointed()
+{
+  lcd.drawBitmap(0,0,disappointed,84,48);
+}
+
+void feelingSad()
+{
+  lcd.drawBitmap(0,0,crying,84,48);
+}
+
+void iAmDead()
+{
+  lcd.drawBitmap(0,0,dead,84,48);
+}
+
+void getMood()
 {
   lcd.clrScr();
-  lcd.drawBitmap(0,0,bad,84,48);
+  if (soilMoisture >= 0 && soilMoisture < 250)
+  {
+    feelingGrumpy();
+  } else if (soilMoisture >= 250 && soilMoisture < 350)
+  {
+    feelingBad();
+  } else if (soilMoisture >= 350 && soilMoisture < 500)
+  {
+    feelingHappy();
+   } else if (soilMoisture >= 500 && soilMoisture < 800)
+  {
+    feelingSad();
+   } else
+  {
+    iAmDead();
+  }
   lcd.update();
-  delay(8000);
-
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,crying,84,48);
-  lcd.update();
-  delay(8000);
-
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,dead,84,48);
-  lcd.update();
-  delay(8000);
-
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,disappointed,84,48);
-  lcd.update();
-  delay(8000);
+}
   
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,grumpy,84,48);
-  lcd.update();
-  delay(8000);
+void loop()
+{
+  readSoilMoisture();
+  getMood();
+  delay(20000);
 
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,happy,84,48);
-  lcd.update();
-  delay(8000);
-
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,sad,84,48);
-  lcd.update();
-  delay(8000);
-
-  lcd.clrScr();
-  lcd.drawBitmap(0,0,wound,84,48);
-  lcd.update();
-  delay(8000);
   
 }
 
